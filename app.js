0cb22bcc90785d22e1bde1c76ae39acbec0cd8c3
Alex Bush <alexabush7@gmail.com>
2018-02-07 10:37:08 -0800

    changed listen method in app.js to listen to process.env.PORT and then default to port 3000

--- app.js
+++ app.js
@@ -9,7 +9,7 @@ app.get("/", function(request, response) {
 });
 
 
-app.listen(3000, function() {
+app.listen(process.env.PORT || 3000, function() {
   console.log(
     "The server is running on port 3000."
   );
